cab809355dd75d636c0fdbdbc62cbb04c50b0397
RIM\Juan.Pablo.Perez <juan.pablo@retailinmotion.com>
2018-03-17 06:32:04 +0000

    Completed App

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import { fetchUsers } from "../actions"
 
-export default class App extends Component {
+class UsersIndex extends Component {
+  componentWillMount() {
+    this.props.fetchUsers()
+  }
+
+  renderUser({ id, name, email }) {
+    return (
+      <li className="list-group-item" key={id}>
+        <span className="label label-defualt label-pill pull-xs-right">
+          <a href={email}>{email}</a>
+        </span>
+        {name}
+      </li>
+    )
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
-    );
+      <div>
+        <h4>Email directory</h4>
+        <ul className="list-group">{this.props.users.map(this.renderUser)}</ul>
+      </div>
+    )
   }
 }
+
+// export default connect(state => state, fetchUsers)(PostsUsers)
+
+function mapStateToProps({ users }) {
+  return { users }
+}
+
+export default connect(mapStateToProps, { fetchUsers })(UsersIndex)
